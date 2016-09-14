27318b2c1a86596f8079ff704950e9e4a349a692
JellyLu <634725546@qq.com>
2016-09-14 11:51:49 +0530

    [Palash/Yangjie]Used enum to refactor directions

---
+++ src/rover/Direction.java
@@ -0,0 +1,40 @@
+package rover;
+
+public enum  Direction {
+    NORTH("N"),
+    EAST("E"),
+    SOUTH("S"),
+    WEST("W");
+
+    static {
+        NORTH.left = WEST;
+        NORTH.right = EAST;
+        EAST.left = NORTH;
+        EAST.right = SOUTH;
+        SOUTH.left = EAST;
+        SOUTH.right = WEST;
+        WEST.left =  SOUTH;
+        WEST.right = NORTH;
+    }
+
+    private String symbol;
+    private Direction left;
+    private Direction right;
+    Direction(String symbol) {
+        this.symbol = symbol;
+    }
+
+
+
+    public Direction getLeft(){
+        return this.left;
+    }
+
+    public Direction getRight() {
+        return right;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}
